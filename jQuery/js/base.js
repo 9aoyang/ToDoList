50531f7b2a152f3c107aee73e0f749fc97b6678f
9aoyang <9aoyang@gmail.com>
2018-10-06 10:50:35 +0800

    page detail optimize

--- jQuery/js/base.js
+++ jQuery/js/base.js
@@ -2,7 +2,7 @@
  * @Author: silence1amb
  * @Date: 2018-10-04 16:12:34
  * @Last Modified by: silence1amb
- * @Last Modified time: 2018-10-04 16:38:07
+ * @Last Modified time: 2018-10-06 10:49:22
  */
 (function () {
   'use strict';
@@ -14,13 +14,14 @@
 
   $form_add_task.on('submit', function (e) {
     e.preventDefault();
-    let inputValue = $(this).find('input[name=content]').val()
+    let $inputValue = $(this).find('input[name=content]')
     let newTask = {};
-    newTask.content = inputValue;
+    newTask.content = $inputValue.val();
     if (!newTask) {
       return;
     }
     addTask(newTask)
+    $inputValue.val(null)
     init(taskList)
   });
   function init(taskList) {
@@ -49,8 +50,10 @@
       <div class="task-item">
         <span><input type="checkbox" name="" id=""></span>
         <span class="task-content">${data.content}</span>
-        <span>删除</span>
-        <span>详细</span>
+        <span class="action-wrap ">
+          <span class="action">删除</span>
+          <span class="action">详细</span>
+        </span>
       </div>
     `
     return $(taskItemTpl)
