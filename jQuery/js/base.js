ddef0f43bb4474f60c868244d4a5af48712dfe24
9aoyang <9aoyang@gmail.com>
2018-10-06 11:57:13 +0800

    [ADD] delete function realize

--- jQuery/js/base.js
+++ jQuery/js/base.js
@@ -2,7 +2,7 @@
  * @Author: silence1amb
  * @Date: 2018-10-04 16:12:34
  * @Last Modified by: silence1amb
- * @Last Modified time: 2018-10-06 10:49:22
+ * @Last Modified time: 2018-10-06 11:52:12
  */
 (function () {
   'use strict';
@@ -11,28 +11,44 @@
   let taskList = store.get('taskList') || [];
 
   init(taskList);
-
   $form_add_task.on('submit', function (e) {
+
     e.preventDefault();
     let $inputValue = $(this).find('input[name=content]')
     let newTask = {};
     newTask.content = $inputValue.val();
-    if (!newTask) {
+    if (!newTask.content) {
+      console.log('任务不能为空');
       return;
     }
     addTask(newTask)
     $inputValue.val(null)
-    init(taskList)
   });
+
   function init(taskList) {
     if (taskList.length) {
       renderTaskList()
     }
   }
 
+  // 刷新localStorage数据并重新渲染页面
+  function updateTaskList() {
+    store.set('taskList', taskList)
+    renderTaskList()
+  }
+
   function addTask(newTask) {
     taskList.push(newTask)
-    store.set('taskList', taskList)
+    updateTaskList()
+  }
+
+  function deleteTask(index) {
+    // 如果没有index或index不存在，直接返回
+    if (!index || !taskList[index]) {
+      return
+    }
+    delete taskList[index]
+    updateTaskList()
   }
 
   function renderTaskList() {
@@ -40,19 +56,31 @@
     let $taskList = $('.task-list')
     $taskList.html('')
     for (let i = 0; i < taskList.length; i++) {
-      const $task = renderTaskItem(taskList[i])
+      const $task = renderTaskItem(taskList[i], i)
       $taskList.append($task)
     }
+
+
+    let $delete_task = $('.delete')
+    let $detail_task = $('detail')
+
+    $delete_task.on('click', function() {
+      let $item = $(this).parent().parent()
+      confirm('确定删除？') ? deleteTask($item.data('index')) : null
+    })
+
   }
 
-  function renderTaskItem(data) {
+  function renderTaskItem(data, index) {
+    if (!data || !index)
+    return
     let taskItemTpl = `
-      <div class="task-item">
+      <div class="task-item" data-index=${index}>
         <span><input type="checkbox" name="" id=""></span>
         <span class="task-content">${data.content}</span>
         <span class="action-wrap ">
-          <span class="action">删除</span>
-          <span class="action">详细</span>
+          <span class="action delete">删除</span>
+          <span class="action detail">详细</span>
         </span>
       </div>
     `
